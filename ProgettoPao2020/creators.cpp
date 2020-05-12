be299cdb0147502df18c78430e124111457722ea
gianpics <7620894+gianpics@users.noreply.github.com>
2020-05-12 11:14:05 +0200

    Modificati classe content e creators

--- ProgettoPao2020/creators.cpp
+++ ProgettoPao2020/creators.cpp
@@ -1,6 +1,9 @@
 #include "creators.h"
 
-Creators::Creators()
-{
+Creators::Creators(Creator& _c, Creators* _prev=nullptr, Creators* _next=nullptr):c(_c),prev(_prev),next(_next){}
 
+Creators::~Creators(){
+    prev=next;
+
+    next->prev=prev;
 }
--- ProgettoPao2020/creators.h
+++ ProgettoPao2020/creators.h
@@ -1,11 +1,18 @@
 #ifndef CREATORS_H
 #define CREATORS_H
-
+#include <creator.h>
 
 class Creators
 {
-public:
-    Creators();
+    private:
+        Creator c;
+        Creators* prev;
+        Creators* next;
+
+    public:
+        Creators(Creator&, Creators*, Creators*);
+        ~Creators();
+
 };
 
 #endif // CREATORS_H
