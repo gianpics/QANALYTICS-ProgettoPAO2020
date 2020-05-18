8a80b5746f5f76f473652839551e496a9ed85d5e
Panz99 <giacomo.sassaro@gmail.com>
2020-05-18 10:20:34 +0200

    Fix bug

--- ProgettoPao2020/stats.cpp
+++ ProgettoPao2020/stats.cpp
@@ -3,12 +3,17 @@
 Stats::Stats(u_int _month, u_int _year, u_int _impression, u_int _coverage, u_int _like) : impression(_impression), coverage(_coverage), like(_like)
 {
     month = (_month>0 && _month<13) ? _month : 0;
-    year = (_year>2000) ? _month : 0;
+    year = (_year>2000) ? _year : 0;
 }
 
 Stats::~Stats(){}
 
 u_int Stats::getMonth() const {return month;}
+
+void Stats::setYear(u_int y)
+{
+    year=y;
+}
 
 u_int Stats::getYear() const {return year;}
 
