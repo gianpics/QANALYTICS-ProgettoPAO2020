2d147307a44dd220d3731e2ce34c978108c75187
gianpics <7620894+gianpics@users.noreply.github.com>
2020-05-20 12:29:26 +0200

    Aggiunta landingwindow

---
+++ ProgettoPao2020/landingwindow.h
@@ -0,0 +1,14 @@
+#ifndef LANDINGWINDOW_H
+#define LANDINGWINDOW_H
+
+#include <QObject>
+#include <QWidget>
+
+class landingwindow
+{
+    Q_OBJECT
+public:
+    landingwindow();
+};
+
+#endif // LANDINGWINDOW_H
