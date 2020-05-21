9acbd9101bbb41bdbedca3729899b825d50aeaa7
gianpics <7620894+gianpics@users.noreply.github.com>
2020-05-21 19:50:43 +0200

    Progressi su landinwindow, aggiunto stylesheet

--- ProgettoPao2020/landingwindow.cpp
+++ ProgettoPao2020/landingwindow.cpp
@@ -1,40 +1,109 @@
 #include <QObject>
+#include <QDebug>
 #include <QSettings>
-#include <QCoreApplication>
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QFile>
 #include "landingwindow.h"
 
 LandingWindow::LandingWindow()
 {
-    //QSettings settings(QString(":/config.ini"), QSettings::IniFormat);
-    setWindowTitle("title");
+    // carica titolo finestra dal file config
+    QSettings settings(QString(":resources/config.ini"), QSettings::IniFormat);
+    setWindowTitle(settings.value("app/title").toString());
 
     // spawn widget
     setWidgets();
 
-    // imposta stili
-    setStyle();
+    // stile
+    setWinStyle();
 }
 
 void LandingWindow::setWidgets(){
+
     // btn impostazioni
-    settingBtn=new QPushButton(this);
-    settingBtn->setIcon(QIcon(":/resources/gear.png"));
-    settingBtn->setFlat(true);
-    connect(settingBtn,SIGNAL(QPushButton::clicked), this, SLOT(settingBtnClick()));
+    settingBtn=new QPushButton;
+    connect(settingBtn,SIGNAL(clicked()), this, SLOT(settingBtnClick()));
 
     // btn info
-    infoBtn=new QPushButton(this);
-    infoBtn->setIcon(QIcon(":/resources/info.png"));
-    infoBtn->setFlat(true);
-    connect(infoBtn,SIGNAL(QPushButton::clicked), this, SLOT(infoBtnClick()));
+    infoBtn=new QPushButton;
+    connect(infoBtn, SIGNAL(clicked()), SLOT(infoBtnClick()));
+
+    // inserimento buttons in layout superiore
+    buttonsLyt=new QHBoxLayout;
+    buttonsLyt->addWidget(infoBtn);
+    buttonsLyt->addWidget(settingBtn);
+    buttonsLyt->setAlignment(Qt::AlignRight);
+
+    // search box
+    searchTxt=new QLineEdit;
+    connect(searchTxt,SIGNAL(textChanged(const QString&)),SLOT(searchTxtChanged(const QString&)));
+
+    // creator list
+    creatorsLvw=new QListView;
+
+    // inserimento in layout inferiore
+    creatorsLyt=new QVBoxLayout;
+    creatorsLyt->addWidget(searchTxt);
+    creatorsLyt->addWidget(creatorsLvw);
+
+    mainLyt=new QVBoxLayout(this);
+    mainLyt->addLayout(buttonsLyt);
+    mainLyt->addLayout(creatorsLyt);
 }
 
-void LandingWindow::setStyle(){}
+void LandingWindow::setWinStyle(){
+
+    // imposta foglio di stile
+    QFile file(":/resources/stylesheet.css");
+    file.open(QFile::ReadOnly);
+    setStyleSheet(QLatin1String(file.readAll()));
+
+    // finestra
+    setFixedSize(QSize(800,600));
+    setGeometry(
+        QStyle::alignedRect(
+            Qt::LeftToRight,
+            Qt::AlignCenter,
+            size(),
+            qApp->desktop()->availableGeometry()
+        )
+    );
+
+    // pulsanti
+    settingBtn->setIcon(QIcon(":/resources/gear.png"));
+    settingBtn->setFlat(true);
+    settingBtn->setToolTip("Settings");
+    settingBtn->setFixedSize(QSize(26,26));
+
+    infoBtn->setIcon(QIcon(":/resources/info.png"));
+    infoBtn->setFlat(true);
+    infoBtn->setToolTip("Information");
+    infoBtn->setFixedSize(QSize(26,26));
+
+    searchTxt->setPlaceholderText("Search");
+
+}
 
 void LandingWindow::settingBtnClick(){
     // visualizza settingsWindow
+    qInfo()<<"click btnsetting";
 }
 
 void LandingWindow::infoBtnClick(){
     // visualizza infoWindow
+    qInfo()<<"click btninfo";
+}
+
+void LandingWindow::searchTxtChanged(const QString &text){
+    // filtra elementi creatorsLyt
+    qInfo()<<"txt: "<<text;
+    if(text=="")
+    {
+        // mostra tutti risultati
+    }
+    else
+    {
+        // regex su nome creator
+    }
 }
--- ProgettoPao2020/landingwindow.h
+++ ProgettoPao2020/landingwindow.h
@@ -4,28 +4,36 @@
 #include <QObject>
 #include <QWidget>
 #include <QPushButton>
-#include <QTextEdit>
+#include <QLineEdit>
 #include <QListView>
 #include <QSettings>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 class LandingWindow: public QWidget
 {
     Q_OBJECT
+
 private:
+    QVBoxLayout *mainLyt;
+    QHBoxLayout *buttonsLyt;
+    QVBoxLayout *creatorsLyt;
     QPushButton *infoBtn, *settingBtn;
-    QTextEdit *searchTxt;
+    QLineEdit *searchTxt;
     QListView *creatorsLvw;
 
     void setWidgets();
-    void setStyle();
+    void setWinStyle();
 
+private slots:
+    void settingBtnClick();
+    void infoBtnClick();
+    void searchTxtChanged(const QString&);
 
 public:
     LandingWindow();
 
-public slots:
-    void settingBtnClick();
-    void infoBtnClick();
+
 };
 
 #endif // LANDINGWINDOW_H
