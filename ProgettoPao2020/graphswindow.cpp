75dd6832f4fda5519e71637e4f2cbc680afe23de
gianpics <7620894+gianpics@users.noreply.github.com>
2020-05-22 21:32:30 +0200

    Aggiunti graphswindow e immagini

---
+++ ProgettoPao2020/graphswindow.cpp
@@ -0,0 +1,75 @@
+#include "graphswindow.h"
+#include <QFile>
+#include <QDebug>
+
+graphswindow::graphswindow()
+{
+    setWidget();
+
+    setWinStyle();
+}
+
+void graphswindow::setWidget()
+{
+    setSideWidget();
+
+    vLine=new QFrame;
+
+    setGraphsWidget();
+
+    mainLyt=new QHBoxLayout(this);
+    mainLyt->addLayout(sideLyt);
+    mainLyt->addWidget(vLine);
+    mainLyt->addLayout(graphsLyt);
+}
+
+void graphswindow::setSideWidget()
+{
+    accountsLbl=new QLabel;
+    accountsLvw=new QListView;
+    hLine=new QFrame;
+    statsLbl=new QLabel;
+    statsLvw=new QListView;
+    sideLyt=new QVBoxLayout;
+    sideLyt->addWidget(accountsLbl);
+    sideLyt->addWidget(accountsLvw);
+    sideLyt->addWidget(hLine);
+    sideLyt->addWidget(statsLbl);
+    sideLyt->addWidget(statsLvw);
+}
+
+void graphswindow::setGraphsWidget()
+{
+    graphsLvw=new QListView;
+
+    graphsLyt=new QVBoxLayout;
+    graphsLyt->addWidget(graphsLvw);
+}
+
+void graphswindow::setWinStyle()
+{
+    // imposta foglio di stile
+    QFile file(":/resources/stylesheet.css");
+    file.open(QFile::ReadOnly);
+    setStyleSheet(QLatin1String(file.readAll()));
+    //qInfo()<<"style: "<<QLatin1String(file.readAll());
+
+    // titolo finestra da nome creator
+    setWindowTitle("temp");
+    setMinimumSize(QSize(600,400));
+
+    accountsLbl->setText("Accounts");
+    accountsLbl->setObjectName("section");
+    statsLbl->setText("Stats");
+    statsLbl->setObjectName("section");
+
+    hLine->setFixedHeight(1);
+    hLine->setObjectName("line");
+    hLine->setFrameShape(QFrame::HLine);
+    hLine->setFrameShadow(QFrame::Sunken);
+
+    vLine->setFixedWidth(1);
+    vLine->setObjectName("line");
+    vLine->setFrameShape(QFrame::VLine);
+    vLine->setFrameShadow(QFrame::Sunken);
+}
